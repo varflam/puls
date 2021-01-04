0c6e68511d4403f8c199fc2a3f220859cb4d6751
Maria <alece.1414@gmail.com>
2021-01-04 18:57:50 +0300

    Tiny slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,35 @@
-$(document).ready(function(){
-  $('.carousel__inner').slick(
-    {
-      speed: 300,
-      prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-left.svg"></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow-right.svg"></button>',
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            dots: true,
-            arrows: false
-          }
-        }
-      ]
-    }
-  );
+// $(document).ready(function(){
+//   $('.carousel__inner').slick(
+//     {
+//       speed: 300,
+//       prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow-left.svg"></button>',
+//       nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow-right.svg"></button>',
+//       responsive: [
+//         {
+//           breakpoint: 768,
+//           settings: {
+//             dots: true,
+//             arrows: false
+//           }
+//         }
+//       ]
+//     }
+//   );
+// });
+
+const slider = tns ({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false
+});
+
+document.querySelector('.prev').addEventListener ('click', function () {
+  slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener ('click', function () {
+  slider.goTo('next');
 });
