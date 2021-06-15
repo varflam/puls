8b29cbff76e60dd32ef1b91a1acac8a46ee78713
Maria <alece.1414@gmail.com>
2021-06-15 15:20:14 +0300

    bug fix and service script add

--- src/js/script.js
+++ src/js/script.js
@@ -1,100 +1,102 @@
-const moreInfo = document.querySelectorAll('.catalog-item__more'),
+window.addEventListener('DOMContentLoaded', () => {
+  //More Button
+  const moreInfo = document.querySelectorAll('.catalog-item__more'),
       moreInfoLink = document.querySelectorAll('.catalog-item__link'),
       firstInfo = document.querySelectorAll('.catalog-item__first'),
-      backLink = document.querySelectorAll('.catalog-item__link_back'),
-      catalog = document.querySelector('.catalog');
+      backLink = document.querySelectorAll('.catalog-item__link_back');
 
-const changeInfo = function(i) {
-  moreInfo[i].classList.toggle('hidden');
-  firstInfo[i].classList.toggle('hidden');
-  firstInfo[i].classList.add('fade');
-  moreInfo[i].classList.add('fade');
-};
-
-
-moreInfoLink.forEach((item, i) => {
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
-    changeInfo(i);
-  });
-});
-
-backLink.forEach((item, i) => {
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
-    changeInfo(i);
-  });
-});
-
-const buttonCons = document.querySelectorAll('.button_cons'),
-      buttonBuy = document.querySelectorAll('.button_catalog'),
-      buttonFinish = document.querySelectorAll('[data-modal="finish"]'),
-      modalCons = document.querySelector('#consultation'),
-      modalClose = document.querySelectorAll('.modal__close'),
-      overlay = document.querySelector('.overlay'),
-      modalBuy = document.querySelector('#order'),
-      modalThanks = document.querySelector('#thanks'),
-      modalWin = document.querySelectorAll('.modal'),
-      modalName = document.querySelector('.modal__name'),
-      itemName = document.querySelectorAll('.catalog-item__subtitle');
+  const changeInfo = function(i) {
+    moreInfo[i].classList.toggle('hidden');
+    firstInfo[i].classList.toggle('hidden');
+    firstInfo[i].classList.add('fade');
+    moreInfo[i].classList.add('fade');
+  };
 
 
-let showModal = function(btns, modal) {
-  btns.forEach((btn, i) => {
-    btn.addEventListener('click', (e) => {
+  moreInfoLink.forEach((item, i) => {
+    item.addEventListener('click', (e) => {
       e.preventDefault();
-      let openModal = function() {
-        modal.style.display = 'block';
-        overlay.style.display = 'block';
-        document.body.style.overflow = 'hidden';
-        modalName.textContent = itemName[i].textContent;
-        modal.classList.add('fade');
-        };
-        openModal(modal);
+      changeInfo(i);
     });
   });
-};
 
-showModal(buttonCons, modalCons);
-showModal(buttonBuy, modalBuy);
-showModal(buttonFinish, modalThanks);
-
-let closeModal = function() {
-  modalWin.forEach(modal => {
-    modal.style.display = 'none';
+  backLink.forEach((item, i) => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      changeInfo(i);
+    });
   });
-  overlay.style.display = 'none';
-  document.body.style.overflow = '';
-};
+  
+  //Modals
 
-modalClose.forEach(item => {
-  item.addEventListener('click', () => {
-    closeModal();
+  const modalClose = document.querySelectorAll('.modal__close'),
+        overlay = document.querySelector('.overlay'),
+        modalWin = document.querySelectorAll('.modal'),
+        modalName = document.querySelector('.modal__name'),
+        itemName = document.querySelectorAll('.catalog-item__subtitle');
+
+
+  let openModal = function (modal, i) {
+    const modalWin = document.querySelector(modal);
+    modalWin.style.display = 'block';
+    overlay.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+    if (i) {
+      modalName.textContent = itemName[i].textContent;
+    }
+    modalWin.classList.add('fade');
+  };
+
+  let showModal = function(btns, modal) {
+    const modalBtns = document.querySelectorAll(btns);
+    const modalWin = document.querySelector(modal);
+    modalBtns.forEach((btn, i) => {
+      btn.addEventListener('click', (e) => {
+        e.preventDefault();
+          openModal(modalWin, i);
+      });
+    });
+  };
+
+  showModal('.button_cons', '#consultation');
+  showModal('.button_catalog', '#order');
+
+  let closeModal = function () {
+    modalWin.forEach(modal => {
+      modal.style.display = 'none';
+    });
+    overlay.style.display = 'none';
+    document.body.style.overflow = '';
+  };
+
+  modalClose.forEach(item => {
+    item.addEventListener('click', () => {
+      closeModal();
+    });
   });
-});
 
-overlay.addEventListener('click', (evt) => {
-  if (evt.target === overlay || evt.target.getAttribute('data-close') == '') {
-    closeModal();
-}
-});
-
-document.addEventListener('keydown', (evt) => {
-  if (evt.code === 'Escape' && overlay.classList.contains('visible')) {
+  overlay.addEventListener('click', (evt) => {
+    if (evt.target === overlay || evt.target.getAttribute('data-close') == '') {
       closeModal();
   }
+  });
+
+  document.addEventListener('keydown', (evt) => {
+    if (evt.code === 'Escape' && overlay.classList.contains('visible')) {
+        closeModal();
+    }
 });
 
+//Slider
 
-const slides = document.querySelectorAll('.slide'),
-        slide = document.querySelector('.carousel'),
-        prev = document.querySelector('.prev'),
-        next = document.querySelector('.next'),
-        sliderWrapper = document.querySelector('.carousel__container'),
-        sliderInner = document.querySelector('.carousel__inner'),
-        width = window.getComputedStyle(sliderWrapper).width,
-        maxOffset = +width.slice(0, width.length - 2);
-        
+  const slides = document.querySelectorAll('.slide'),
+          prev = document.querySelector('.prev'),
+          next = document.querySelector('.next'),
+          sliderWrapper = document.querySelector('.carousel__container'),
+          sliderInner = document.querySelector('.carousel__inner'),
+          width = window.getComputedStyle(sliderWrapper).width,
+          maxOffset = +width.slice(0, width.length - 2);
+          
     let slideIndex = 1,
         offset = 0;
 
@@ -137,3 +139,32 @@ const slides = document.querySelectorAll('.slide'),
             slideIndex--;
         }
     });
+  
+    //Forms
+    const forms = document.querySelectorAll('form');
+
+    const postData = async (url, data) => {
+      let res = await fetch(url, {
+          method: 'POST',
+          body: data
+      });
+      
+      return await res;
+    };
+
+    forms.forEach(form => {
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formData = new FormData(form);
+
+            postData('js/mailer/smart.php', formData)
+            .then(() => {
+                openModal('#thanks');
+            }).finally(() => {
+                setInterval(closeModal, 2000);
+            });
+      });
+    });
+
+});
+
